581e880c758b7f61538a7288ce3a455c032c534b
Dayane Leal <dayane.cbleal@gmail.com>
2022-05-27 22:35:38 -0300

    Jwt

--- src/main/java/br/com/app_cadastro/security/CredenciaisContaVO.java
+++ src/main/java/br/com/app_cadastro/security/CredenciaisContaVO.java
@@ -2,6 +2,9 @@ package br.com.app_cadastro.security;
 
 import java.io.Serializable;
 
+import lombok.Data;
+
+
 public class CredenciaisContaVO implements Serializable {
 
 	/**
@@ -9,7 +12,23 @@ public class CredenciaisContaVO implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 	
+
 	private String userName;
 	private String password;
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
 
 }
